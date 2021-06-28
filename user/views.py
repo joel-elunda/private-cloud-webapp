5de5ca157e007f0dfd51b6232ef1b04ed9374288
Joël Elunda <joel.elunda.01@gmail.com>
2021-06-28 07:08:20 +0200

    django generic form

--- user/views.py
+++ user/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
+from user.forms import UserLogin, ContactForm
 
 def password_change(request):
     if request.method == 'POST':
@@ -22,9 +23,25 @@ def email_check(user):
 @login_required  
 @user_passes_test(email_check, login_url='/login/')
 def login(request):
+    if request.method == 'POST':
+        form = UserLogin(request.POST)
+    if form.is_valid():
+        pass  # does nothing, just trigger the validation
+    else:
+        form = UserLogin()
     return render(request, 'login.html')
 
 
+
+
+def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass  # does nothing, just trigger the validation
+    else:
+        form = ContactForm()
+    return render(request, 'index.html', {'form': form})
 
 class LoginCreateView(CreateView):
     pass
